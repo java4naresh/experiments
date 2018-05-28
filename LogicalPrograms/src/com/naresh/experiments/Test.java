0a133c8f6d0aad9594d6ea8f9005b390f5c7d795
java4naresh <kambalanaresh4java@gmail.com>
2018-05-28 23:30:57 +0530

    added arrays project

--- LogicalPrograms/src/com/naresh/experiments/Test.java
+++ LogicalPrograms/src/com/naresh/experiments/Test.java
@@ -1,9 +1,31 @@
 package com.naresh.experiments;
 
+import java.util.Scanner;
+
 public class Test {
 	
 	public static void main(String[] args) {
-		String name="verra";
+		int n,m,a=0;
+		System.out.println("enter any value");
+		Scanner sc=new Scanner(System.in);
+	 n	=sc.nextInt();
+	 m=n;
+	while(n>0)
+	{
+		int x=n%10;
+		 a=a*10+x;
+		 n=n/10;
+		
+	}
+	if(a==m)
+	{
+		System.out.println("enter value====="+m+"==it is palandrom");
+	}
+	else
+	{
+		System.out.println("enter value is======"+m+"==it is not palandrom");
+	}
+				
 	}
 
 }
---
+++ arrays/src/arrays/single_dymanisal.java
@@ -0,0 +1,35 @@
+package arrays;
+
+public class single_dymanisal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         /*  int[] a= {1,3,4,5,6,7};
+           System.out.println(a[0]);
+           System.out.println(a[1]);
+           System.out.println(a[2]);
+           System.out.println(a[3]);
+           System.out.println(a[4]);*/
+		/*int[] b=new int[5];
+		System.out.println(b[0]);
+		System.out.println(b[1]);
+		System.out.println(b[2]); 
+		System.out.println(b[3]);
+		System.out.println(b[4]);
+	
+		*/
+		String[] str=new String[5];
+		str[0]="veera";
+		str[1]="naresh";
+		str[2]="babu";
+		str[3]="ramesh";
+		str[4]="bhaskar";
+		System.out.println(str[0]);
+		System.out.println(str[1]);
+		System.out.println(str[2]);
+		System.out.println(str[3]);
+		System.out.println(str[4]);
+		
+	}
+
+}
